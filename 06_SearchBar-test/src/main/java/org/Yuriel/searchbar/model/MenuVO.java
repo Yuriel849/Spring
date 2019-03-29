21c97f49b0a0b843c90d39fedfda11ff26e49025
Yuriel849 <yuriel849sp@gmail.com>
2019-03-29 11:09:58 +0900

    VO 추가 (kitchen, biz, menu 읽어오려면 필요)

---
+++ 06_SearchBar-test/src/main/java/org/Yuriel/searchbar/model/MenuVO.java
@@ -0,0 +1,12 @@
+package org.Yuriel.searchbar.model;
+
+import lombok.Data;
+
+@Data
+public class MenuVO {
+	private int idx;
+	private String name;
+	private int basicPrice;
+	private String photo;
+	private int categoryId; // menuCatCode
+}
