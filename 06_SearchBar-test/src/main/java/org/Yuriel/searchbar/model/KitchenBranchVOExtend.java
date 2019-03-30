b73effeff270fad2b3e272581ed0bbef060b6c90
Yuriel849 <yuriel849sp@gmail.com>
2019-03-30 17:41:20 +0900

    기존 VO 대신 VOExtend로 변경

---
+++ 06_SearchBar-test/src/main/java/org/Yuriel/searchbar/model/KitchenBranchVOExtend.java
@@ -0,0 +1,10 @@
+package org.Yuriel.searchbar.model;
+
+import lombok.Data;
+
+@Data
+public class KitchenBranchVOExtend {
+	private String kitchenid;
+	private String kitchenname;
+	private String kitchenaddress;
+}
